6ca2a86f3a6e541e4273461406d4f735b1b577eb
Nafi Robayat <nafi@live.com>
2025-06-22 15:37:18 +1000

    Added ignored_folders.lua

---
+++ lua/custom/utilities/ignored_folders.lua
@@ -0,0 +1,13 @@
+return {
+    ".git/",
+    "node_modules/",
+    "dist/",
+    "build/",
+    "coverage/",
+    "vendor/",
+    "tmp/",
+    "temp/",
+    ".cache/",
+    "stm32cube/",
+    "External/",
+}
