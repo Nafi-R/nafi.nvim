b9fdecea15774bde1c5097b9aa57260ac4b90887
Nafi Robayat <nafi@live.com>
2025-07-05 14:23:14 +1000

    Modularised all plugins

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,18 @@
 local map = vim.keymap.set
 
 map('n', '<leader>cc', require('custom.utilities.compile_commands').copy_compile_commands, { desc = '[C]opy [C]ompile commands to root directory' })
+map('n', '<esc><esc>', '<C-\\><C-n>', { desc = 'Exit to normal mode' })
+map('n', '<leader>`', '<CMD>Floaterminal<CR>', { desc = 'Toggle Floating Terminal' })
+
+map('n', '<C-h>', '<C-w><C-h>', {
+  desc = 'Move focus to the left window',
+})
+map('n', '<C-l>', '<C-w><C-l>', {
+  desc = 'Move focus to the right window',
+})
+map('n', '<C-j>', '<C-w><C-j>', {
+  desc = 'Move focus to the lower window',
+})
+map('n', '<C-k>', '<C-w><C-k>', {
+  desc = 'Move focus to the upper window',
+})
