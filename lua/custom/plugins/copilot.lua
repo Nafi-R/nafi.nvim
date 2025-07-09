753121583c697e676b75220c01d4a3f228d41022
Nafi Robayat <nafi@live.com>
2025-07-10 00:23:19 +1000

    Adjusted copilot inline hints settings

--- lua/custom/plugins/copilot.lua
+++ lua/custom/plugins/copilot.lua
@@ -1,6 +1,24 @@
 return {
   {
     'github/copilot.vim',
+    config = function()
+      -- Set semi-transparent inline hints
+      vim.api.nvim_set_hl(0, 'CopilotSuggestion', {
+        fg = '#555555', -- Adjust color for semi-transparency
+        ctermfg = 8,
+      })
+
+      -- Clear Copilot suggestions when entering normal mode
+      vim.api.nvim_create_autocmd('ModeChanged', {
+        pattern = '*:n',
+        callback = function()
+          vim.fn['copilot#Dismiss']()
+        end,
+      })
+
+      -- Set enterprise GitHub link
+      -- vim.g.copilot_enterprise_uri = 'https://DOMAIN.ghe.com'
+    end,
   },
   {
     {
