701b4b37ca54b55a52ffe9dbb2a61f7ab007b72d
Nafi Robayat <nafi@live.com>
2025-11-29 23:15:52 +1100

    Fixed autosessions saving correctly on exit

--- lua/custom/plugins/autosessions.lua
+++ lua/custom/plugins/autosessions.lua
@@ -7,6 +7,20 @@ return {
   ---@type AutoSession.Config
   opts = {
     suppressed_dirs = { '~/', '~/Projects', '~/Downloads', '/' },
-    -- log_level = 'debug',
-  }
+  },
+  config = function(_, opts)
+    require('auto-session').setup(opts)
+    vim.o.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
+
+    local sessionexit = vim.api.nvim_create_augroup('sessionexit', {
+      clear = true,
+    })
+    -- Save session when leaving neovim
+    vim.api.nvim_create_autocmd({ 'VimLeavePre' }, {
+      group = sessionexit,
+      callback = function()
+        require('auto-session').auto_save_session()
+      end,
+    })
+  end
 }
