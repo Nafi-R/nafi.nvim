4c7ab9f89d1e91dea9474d357e438fcacb6344ce
Nafi Robayat <nafi@live.com>
2026-02-16 02:08:49 +1100

    Added back oil and added yazi

--- lua/custom/plugins/file-explorer.lua
+++ lua/custom/plugins/file-explorer.lua
@@ -1,57 +1,72 @@
--- return {
---   'stevearc/oil.nvim',
---   ---@module 'oil'
---   ---@type oil.SetupOpts
---   opts = {
---     delete_to_trash = true, -- Use the system trash for deletions
---   },
---   -- Optional dependencies
---   dependencies = { { 'echasnovski/mini.icons', opts = {} } },
---   keys = {
---     { '<leader>e', '<cmd>Oil --float<cr>', desc = 'Open Oil' },
---   },
---   -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
---   -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
---   lazy = false,
--- }
-
 return {
   {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "MunifTanjim/nui.nvim",
-      "nvim-tree/nvim-web-devicons", -- optional, but recommended
+    'stevearc/oil.nvim',
+    ---@module 'oil'
+    ---@type oil.SetupOpts
+    opts = {
+      delete_to_trash = true, -- Use the system trash for deletions
     },
-    lazy = false,                    -- neo-tree will lazily load itself
+    -- Optional dependencies
+    dependencies = { { 'echasnovski/mini.icons', opts = {} } },
     keys = {
       {
-        '<leader>e', '<CMD>Neotree toggle=true reveal=true<CR>', desc = 'Open file explorer'
+        '<leader>e',
+        function()
+          require('oil').toggle_float(nil, {
+            preview = {
+              vertical = true
+            }
+          }, nil)
+        end,
+        desc = 'Toggle Oil (float)'
       },
     },
-    opts = {
-      event_handlers = {
-        {
-          event = "neo_tree_buffer_enter",
-          handler = function()
-            vim.opt_local.number = true
-            vim.opt_local.relativenumber = true
-          end,
-        },
-      },
-      filesystem = {
-        filtered_items = {
-          visible = true,
-          hide_dotfiles = false,
-          hide_gitignored = false,
-        },
-        window = {
-          mappings = {
-            ['-'] = 'navigate_up',
-          },
-        },
-      },
-    },
+    -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
+    -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
+    lazy = false,
   },
+
+  ---@type LazySpec
+  {
+    "mikavilpas/yazi.nvim",
+    version = "*", -- use the latest stable version
+    event = "VeryLazy",
+    dependencies = {
+      { "nvim-lua/plenary.nvim", lazy = true },
+    },
+    keys = {
+      {
+        "<leader>-",
+        mode = { "n", "v" },
+        "<cmd>Yazi<cr>",
+        desc = "Open yazi at the current file",
+      },
+      {
+        -- Open in the current working directory
+        "<leader>cw",
+        "<cmd>Yazi cwd<cr>",
+        desc = "Open the file manager in nvim's working directory",
+      },
+      {
+        "<c-up>",
+        "<cmd>Yazi toggle<cr>",
+        desc = "Resume the last yazi session",
+      },
+    },
+    ---@type YaziConfig | {}
+    opts = {
+      -- if you want to open yazi instead of netrw, see below for more info
+      open_for_directories = false,
+      keymaps = {
+        show_help = "<f1>",
+      },
+    },
+    -- 👇 if you use `open_for_directories=true`, this is recommended
+    init = function()
+      -- mark netrw as loaded so it's not loaded at all.
+      --
+      -- More details: https://github.com/mikavilpas/yazi.nvim/issues/802
+      vim.g.loaded_netrwPlugin = 1
+    end,
+  }
 }
