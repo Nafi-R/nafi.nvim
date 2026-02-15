cdb2c2316452f228fb077ec101a0b4b3aebdda95
Nafi Robayat <nafi@live.com>
2026-02-16 02:24:54 +1100

    Fixed treesitter error

--- lua/custom/plugins/treesitter.lua
+++ lua/custom/plugins/treesitter.lua
@@ -1,9 +1,12 @@
 return { -- Highlight, edit, and navigate code
   'nvim-treesitter/nvim-treesitter',
+  branch = 'main',
   config = function()
     local filetypes = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim',
       'vimdoc' }
-    require('nvim-treesitter').install(filetypes)
+    local ts = require('nvim-treesitter')
+    ts.setup()
+    ts.install(filetypes)
     vim.api.nvim_create_autocmd('FileType', {
       pattern = filetypes,
       callback = function() vim.treesitter.start() end,
