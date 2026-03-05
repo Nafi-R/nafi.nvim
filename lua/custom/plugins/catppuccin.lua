1d68195ce9be92a0ff24d0ef089863e3717f1b8a
Nafi Robayat <nafi@live.com>
2026-03-06 00:27:53 +1100

    Fixed tranparent background issue with theme

--- lua/custom/plugins/catppuccin.lua
+++ lua/custom/plugins/catppuccin.lua
@@ -2,7 +2,11 @@ return {
   'catppuccin/nvim',
   lazy = false,
   config = function()
+    require('catppuccin').setup {
+      transparent_background = true,
+    }
     vim.cmd.colorscheme 'catppuccin-mocha'
+    vim.opt.termguicolors = true
   end,
   name = 'catppuccin',
   priority = 1000,
